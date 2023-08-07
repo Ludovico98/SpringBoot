03601a2ba4c9a9a82645619c005e1bb86b398944
Ludovico Bitti <55153580+Ludovico98@users.noreply.github.com>
2023-08-07 10:14:42 +0100

    Spring boot first attempt
    1 - Running VScode - go to /course-springboot/src/main/java/com/ludo/coursespringboot/HelloController.java
    2 - Test - open browser type http://localhost:8080/hello or /topics
    This is an example of a spring boot application

---
+++ course-springboot/src/main/java/com/ludo/coursespringboot/TopicController.java
@@ -0,0 +1,21 @@
+package com.ludo.coursespringboot;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+    
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics()
+    {
+        return Arrays.asList(
+            new Topic("spirng","Spring Framework","Spring Framework Description"),
+            new Topic("java","Core Java","Core Java Description"),
+            new Topic("javascript","JavaScript","JavaScript Description")
+        );
+    }
+
+}
