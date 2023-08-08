7c070d7a1fad580fba70cf045f10b62cf116e61f
Ludovico Bitti <l.bitti98@gmail.com>
2023-08-08 16:33:24 +0100

    Put / Delete request
    Put  request is working which allows to add items to the topic list
    
    Testing for Delete button is in progress and will need to be tested and improved. i am still learning how to use postman.

--- course-springboot/src/main/java/com/ludo/coursespringboot/TopicService.java
+++ course-springboot/src/main/java/com/ludo/coursespringboot/TopicService.java
@@ -15,17 +15,34 @@ public class TopicService {
         new Topic("javascript","JavaScript","JavaScript Description")
         ));
     
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics()
+    {
         return topics;
     }
 
-    public Topic getTopic(String id){
+    public Topic getTopic(String id)
+    {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-        
     }
 
     public void addTopic(Topic topic){
         topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic)
+    {
+        for(int i = 0; i<topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id))
+            {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id, Topic topic) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 
 }
