d1b09ce81534f0ceca6b166df4cca51123d23ca8
Ludovico Bitti <l.bitti98@gmail.com>
2023-08-10 14:04:51 +0100

    PUT fixed
    PUT functions has a variable that it hold which hols the value for the topic and if there's something in it, get's it, otherwise it will return null

--- course-api-data/src/main/java/io/javabrains/courseapidata/TopicController.java
+++ course-api-data/src/main/java/io/javabrains/courseapidata/TopicController.java
@@ -21,23 +21,32 @@ public class TopicController {
     }
 
     @RequestMapping("/topics/{id}")
-    public Topic getTopic(@PathVariable String id){
+    public Topic getTopic(@PathVariable String id)
+    {
         return topicService.getTopic(id);
     }
 
+    // Adds a topic to the topics list, ID needs to be different to add the topic
     @RequestMapping(method=RequestMethod.POST, value="/topics")
-    public void addTopic(@RequestBody Topic topic){
+    public void addTopic(@RequestBody Topic topic)
+    {
         topicService.addTopic(topic);
     }
 
+    // Updates the topic i.e. topic name: Marco -> name: Paolo
     @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
-    public void updateTopic(@RequestBody Topic topic , @PathVariable String id){
+    public void updateTopic(@RequestBody Topic topic , @PathVariable String id)
+    {
         topicService.updateTopic(id, topic);
     }
 
+    // Removes topic from the list of topics
     @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
-    public void deleteTopic(@PathVariable String id){
+    public void deleteTopic(@PathVariable String id)
+    {
         topicService.deleteTopic(id);
     }
+
+
  
 }
--- course-api-data/src/main/java/io/javabrains/courseapidata/TopicService.java
+++ course-api-data/src/main/java/io/javabrains/courseapidata/TopicService.java
@@ -3,6 +3,7 @@ package io.javabrains.courseapidata;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,19 @@ public class TopicService {
         List<Topic> topics = new ArrayList<>();
         topicRepository.findAll().forEach(topics::add);
         return topics;
+
     }
 
     public Topic getTopic(String id)
     {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        //return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        Optional<Topic> optionalTopic = topicRepository.findById(id);
+        
+        if(optionalTopic.isPresent())
+            return optionalTopic.get();
+        return null;
+
+
     }
 
     public void addTopic(Topic topic){
@@ -38,18 +47,13 @@ public class TopicService {
 
     public void updateTopic(String id, Topic topic)
     {
-        for(int i = 0; i<topics.size(); i++){
-            Topic t = topics.get(i);
-            if(t.getId().equals(id))
-            {
-                topics.set(i, topic);
-                return;
-            }
-        }
+
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        //topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 
 }
